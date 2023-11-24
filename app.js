1c89b6605819ec63572caa8002cdf1f9d89641b9
yvzklc <kilciyavuz34@gmail.com>
2023-11-24 16:20:56 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//!APİ https://anthonyfs.pythonanywhere.com/api/products/
+
+//!shopping cart dom selectors
+const shopCart = document.querySelector(".offcanvas-body")
+const cartProductTotal = document.querySelector(".cartProductTotal")
+const cartProductAmount = document.querySelector(".cartProductAmount")
+const cartProductName = document.querySelector(".cartProductName")
+const cartProductImg = document.querySelector(".cartProductImg")
+const degreeBtn = document.querySelector(".degreeBtn")
+const increaceBtn = document.querySelector(".increaseBtn")
+const removeBtn = document.querySelector(".removeBtn")
+const totalPrice = document.querySelector("#totalPrice")
+//!Filter attributes dom selectors
+const searchInput = document.querySelector("#searchInput")
+const category = document.querySelector("#category")
+//!products dom selectors
+const products = document.querySelector("#products")
+const productName = document.querySelector(".productName")
+const productDesc = document.querySelector(".productDesc")
+const productImg = document.querySelector(".productImg")
+const productPrice = document.querySelector(".productPrice")
+const addCart = document.querySelector(".addCart")
+const seeDetails = document.querySelector(".seeDetails")
+//!Modal dom selectors
+const modalName = document.querySelector(".modalName")
+const modalBody = document.querySelector(".modalBody")
+
