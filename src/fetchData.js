9596160e7018862d3a5aaa0f0c25e383bc8175e2
yvzklc <kilciyavuz34@gmail.com>
2023-11-25 17:34:50 +0300

    filter category added

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,62 +1,100 @@
-var API= "https://anthonyfs.pythonanywhere.com/api/products/"
-const shopCard = document.querySelector(".offcanvas-body")
-const cartProductTotal = document.querySelector(".cartProductTotal")
-const cartProductAmount = document.querySelector(".cartProductAmount")
-const cartProductName = document.querySelector(".cartProductName")
-const cartProductImg = document.querySelector(".cartProductImg")
-const degreeBtn = document.querySelector(".degreeBtn")
-const increaceBtn = document.querySelector(".increaseBtn")
-const removeBtn = document.querySelector(".removeBtn")
-const totalPrice = document.querySelector("#totalPrice")
+var API = "https://anthonyfs.pythonanywhere.com/api/products/";
+const shopCard = document.querySelector(".offcanvas-body");
+const cartProductTotal = document.querySelector(".cartProductTotal");
+const cartProductAmount = document.querySelector(".cartProductAmount");
+const cartProductName = document.querySelector(".cartProductName");
+const cartProductImg = document.querySelector(".cartProductImg");
+const degreeBtn = document.querySelector(".degreeBtn");
+const increaceBtn = document.querySelector(".increaseBtn");
+const removeBtn = document.querySelector(".removeBtn");
+const totalPrice = document.querySelector("#totalPrice");
 //!Filter attributes dom selectors
-const searchInput = document.querySelector("#searchInput")
-const category = document.querySelector("#category")
+const searchInput = document.querySelector("#searchInput");
+const category = document.querySelector("#category");
+//!Category buttons dom selectors
+const btns = document.querySelector("#btns");
 //!products dom selectors
-const products = document.querySelector("#products")
-const productName = document.querySelector(".productName")
-const productDesc = document.querySelector(".productDesc")
-const productImg = document.querySelector(".productImg")
-const productPrice = document.querySelector(".productPrice")
-const addCart = document.querySelector(".addCart")
-const seeDetails = document.querySelector(".seeDetails")
+const products = document.querySelector("#products");
+const productName = document.querySelector(".productName");
+const productDesc = document.querySelector(".productDesc");
+const productImg = document.querySelector(".productImg");
+const productPrice = document.querySelector(".productPrice");
+const addCart = document.querySelector(".addCart");
+const seeDetails = document.querySelector(".seeDetails");
 //!Modal dom selectors
-const modalName = document.querySelector(".modalName")
-const modalBody = document.querySelector(".modalBody")
-const getFetch = ()=>{
-    fetch(API)
+const modalName = document.querySelector(".modalName");
+const modalBody = document.querySelector(".modalBody");
+const getFetch = () => {
+  fetch(API)
     .then((res) => {
-        if(!res.ok){
-            throw new error("something went wrong",res.status)
-        }
-        return  res.json()
+      if (!res.ok) {
+        throw new error("something went wrong", res.status);
+      }
+      return res.json();
     })
     .then((data) => showData(data))
-    .catch((err) => console.log(err,res.status))
+    .catch((error) => console.log(error, res.status));
+};
+const showData = (data) => {
+  const [...all] = data;
+  const {
+    id,
+    title,
+    description,
+    category,
+    category_id,
+    price,
+    quantity,
+    image,
+  } = all;
+  var categoryArr = [];
+  all.reduce((acc, ctg) => {
+    if (categoryArr.indexOf(ctg.category) == -1) {
+      categoryArr.push(ctg.category);
+    }
+  });
+  const colors = [
+    "primary",
+    "secondary",
+    "success",
+    "info",
+    "danger",
+    "light",
+    "dark",
+  ];
+  for (let i = 0; i < categoryArr.length; i++) {
+    btns.innerHTML += `<button  class="btn btn-${colors[i]}">${categoryArr[i]}</button>`;
+  }
 
-}
-const showData = (data)=>{
 
-const  [...all] = data
-console.log(all)
-const {id,title,description,category,category_id,price,quantity,image} = all
-for(let i = 0;i<data.length;i++){
-    let shortDesc = all[i].description.split(" ").slice(0,13).join("  ")
-    products.innerHTML += ` <div class="col">
+  const show = (data, length) => {
+    var y = -1;
+    function clearShow() {
+      products.innerHTML = "";
+    }
+    clearShow()
+    for (let i = 0; i < length; i++) {
+    
+
+      y = y + 1;
+
+      let shortDesc = data[i].description.split(" ").slice(0, 13).join("  ");
+      products.innerHTML += ` <div class="col">
         <div class="card h-100">
           <img
-            src="${all[i].image}"
+            src="${data[y].image}"
             class="p-2 productImg"
             height="250px"
             alt="..."
           />
           <div class="card-body ">
-            <h5 class="card-title line-clamp-1 productName">${all[i].title}</h5>
+            <h5 class="card-title line-clamp-1 productName">${data[y].title}</h5>
             <p class="card-text line-clamp-3 productDesc">${shortDesc}...</p>
           </div>
           <div
             class="card-footer w-100  fw-bold d-flex justify-content-between gap-3"
           >
-            <span>Price:</span><span class="productPrice">Price: ${all[i].price}</span>
+            <span>Price:</span><span class="productPrice">Price: ${data[y].price}</span>
           </div>
           <div class="card-footer w-100 d-flex justify-content-center gap-3">
             <button class="btn btn-danger addCart">Sepete Ekle</button>
@@ -69,14 +107,37 @@ for(let i = 0;i<data.length;i++){
             </button>
           </div>
         </div>
-      </div>`
-       
+      </div>`;
     }
+  };
+  btns.addEventListener(
+    "click",
 
+    (e) => {
+      let value = e.target.textContent;
+      let i = 0
+      if (value != "ALL") {
+        i++
+        function clearHTML() {
+          products.innerHTML = "";
+        }
+       
 
-   }
-export const Dataui = ()=>{
-   
-getFetch()    
-    
-}
+        console.log(value);
+        var filtered = data.filter((x) => x.category.includes(value));
+        console.log(filtered);
+        clearHTML();
+
+        show(filtered, filtered.length);
+      }
+      else{
+        show(data, data.length);
+      }
+    }
+  );
+
+  show(data, data.length);
+};
+export const Dataui = () => {
+  getFetch();
+};
